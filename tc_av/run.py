7ebdbc3d9cea6edc00080cb5a54420b4aef16689
Tom Hamilton Stubber <tomhamiltonstubber@gmail.com>
2024-01-17 17:31:43 +0000

    Run clamd before app start

--- tc_av/run.py
+++ tc_av/run.py
@@ -1,10 +1,18 @@
 import os
+import subprocess
 
 import uvicorn
 from app.logs import setup_logging
 from app.main import tc_av_app
 
+
+def start_clamd():
+    subprocess.Popen(['clamd'])
+    subprocess.Popen(['freshclam'])
+
+
 if __name__ == '__main__':
     setup_logging()
+    start_clamd()
     port = int(os.getenv('PORT', 8000))
     uvicorn.run(tc_av_app, host='0.0.0.0', port=port)
